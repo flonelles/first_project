163209b3eab96b53cd2f92c61891f79b1eeb32dd
flonelles <saveliy.ryabov.2019@mail.ru>
2024-03-25 18:13:20 +0500

    задача Дата и время

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import logging
-from telegram.ext import Application, MessageHandler, filters
+from telegram.ext import Application, CommandHandler
+import datetime
 
 BOT_TOKEN = ''
 
@@ -13,14 +14,31 @@ logger = logging.getLogger(__name__)
 
 # Определяем функцию-обработчик сообщений.
 # У неё два параметра, updater, принявший сообщение и контекст - дополнительная информация о сообщении.
-async def echo(update, context):
-    # У объекта класса Updater есть поле message,
-    # являющееся объектом сообщения.
-    # У message есть поле text, содержащее текст полученного сообщения,
-    # а также метод reply_text(str),
-    # отсылающий ответ пользователю, от которого получено сообщение.
-    await update.message.reply_text(f'Я получил сообщение {update.message.text}')
+async def start(update, context):
+    """Отправляет сообщение когда получена команда /start"""
+    user = update.effective_user
+    await update.message.reply_html(
+        rf"Привет {user.mention_html()}! Я эхо-бот. Напишите мне что-нибудь, и я пришлю это назад!",
+    )
 
+
+async def help_command(update, context):
+    """Отправляет сообщение когда получена команда /help"""
+    await update.message.reply_text("Я пока не умею помогать... Я только ваше эхо.")
+
+
+async def time(update, context):
+    await update.message.reply_text(datetime.datetime.now().strftime("%H:%M:%S"))
+
+
+async def date(update, context):
+    await update.message.reply_text(datetime.datetime.now().strftime('%d-%m-%Y'))
+
+
+# Зарегистрируем их в приложении перед
+# регистрацией обработчика текстовых сообщений.
+# Первым параметром конструктора CommandHandler я
+# вляется название команды.
 
 def main():
     # Создаём объект Application.
@@ -32,10 +50,11 @@ def main():
     # После регистрации обработчика в приложении
     # эта асинхронная функция будет вызываться при получении сообщения
     # с типом "текст", т. е. текстовых сообщений.
-    text_handler = MessageHandler(filters.TEXT, echo)
 
-    # Регистрируем обработчик в приложении.
-    application.add_handler(text_handler)
+    application.add_handler(CommandHandler("start", start))
+    application.add_handler(CommandHandler("help", help_command))
+    application.add_handler(CommandHandler('time', time))
+    application.add_handler(CommandHandler('date', date))
 
     # Запускаем приложение.
     application.run_polling()
