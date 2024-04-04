bc09e8d7294411feaeba271b9cdf8a09a081493b
flonelles <saveliy.ryabov.2019@mail.ru>
2024-04-04 17:28:38 +0500

    задача Диалоговый бот

--- main.py
+++ main.py
@@ -1,10 +1,8 @@
 import logging
-from telegram.ext import Application, CommandHandler
-import datetime
+from telegram.ext import Application, CommandHandler, ConversationHandler, MessageHandler, filters
 
-BOT_TOKEN = ''
+BOT_TOKEN = '6610676706:AAEKHc-7_1cn21Fns8FM0Gildh6n3Jx8q30'
 
-# Запускаем логгирование
 logging.basicConfig(
     format = '%(asctime)s - %(name)s - %(levelname)s - %(message)s', level = logging.DEBUG
 )
@@ -12,33 +10,56 @@ logging.basicConfig(
 logger = logging.getLogger(__name__)
 
 
-# Определяем функцию-обработчик сообщений.
-# У неё два параметра, updater, принявший сообщение и контекст - дополнительная информация о сообщении.
 async def start(update, context):
-    """Отправляет сообщение когда получена команда /start"""
-    user = update.effective_user
-    await update.message.reply_html(
-        rf"Привет {user.mention_html()}! Я эхо-бот. Напишите мне что-нибудь, и я пришлю это назад!",
-    )
+    await update.message.reply_text(
+        "Привет. Пройдите небольшой опрос, пожалуйста!\n"
+        "Вы можете прервать опрос, послав команду /stop.\n"
+        "Вы можете пропустить вопрос, послав команду /skip.\n"
+        "В каком городе вы живёте?")
+
+    # Число-ключ в словаре states —
+    # втором параметре ConversationHandler'а.
+    return 1
+    # Оно указывает, что дальше на сообщения от этого пользователя
+    # должен отвечать обработчик states[1].
+    # До этого момента обработчиков текстовых сообщений
+    # для этого пользователя не существовало,
+    # поэтому текстовые сообщения игнорировались.
 
 
-async def help_command(update, context):
-    """Отправляет сообщение когда получена команда /help"""
-    await update.message.reply_text("Я пока не умею помогать... Я только ваше эхо.")
+async def first_response(update, context):
+    # Это ответ на первый вопрос.
+    # Мы можем использовать его во втором вопросе.
+    locality = update.message.text
+    await update.message.reply_text(
+        f"Какая погода в городе {locality}?")
+    # Следующее текстовое сообщение будет обработано
+    # обработчиком states[2]
+    return 2
 
 
-async def time(update, context):
-    await update.message.reply_text(datetime.datetime.now().strftime("%H:%M:%S"))
+async def second_response(update, context):
+    # Ответ на второй вопрос.
+    # Мы можем его сохранить в базе данных или переслать куда-либо.
+    weather = update.message.text
+    logger.info(weather)
+    await update.message.reply_text("Спасибо за участие в опросе! Всего доброго!")
+    return ConversationHandler.END  # Константа, означающая конец диалога.
+    # Все обработчики из states и fallbacks становятся неактивными.
 
 
-async def date(update, context):
-    await update.message.reply_text(datetime.datetime.now().strftime('%d-%m-%Y'))
+async def stop(update, context):
+    await update.message.reply_text("Всего доброго!")
+    return ConversationHandler.END
 
 
-# Зарегистрируем их в приложении перед
-# регистрацией обработчика текстовых сообщений.
-# Первым параметром конструктора CommandHandler я
-# вляется название команды.
+async def skip_city(update, context):
+    await update.message.reply_text(
+        f"Какая погода у вас в городе?")
+    # Следующее текстовое сообщение будет обработано
+    # обработчиком states[2]
+    return 2
+
 
 def main():
     # Создаём объект Application.
@@ -51,11 +72,23 @@ def main():
     # эта асинхронная функция будет вызываться при получении сообщения
     # с типом "текст", т. е. текстовых сообщений.
 
-    application.add_handler(CommandHandler("start", start))
-    application.add_handler(CommandHandler("help", help_command))
-    application.add_handler(CommandHandler('time', time))
-    application.add_handler(CommandHandler('date', date))
+    conv_handler = ConversationHandler(
+        # Точка входа в диалог.
+        # В данном случае — команда /start. Она задаёт первый вопрос.
+        entry_points = [CommandHandler('start', start)],
 
+        # Состояние внутри диалога.
+        # Вариант с двумя обработчиками, фильтрующими текстовые сообщения.
+        states = {
+            # Функция читает ответ на первый вопрос и задаёт второй.
+            1: [MessageHandler(filters.TEXT & ~filters.COMMAND, first_response), CommandHandler('skip', skip_city)],
+            # Функция читает ответ на второй вопрос и завершает диалог.
+            2: [MessageHandler(filters.TEXT & ~filters.COMMAND, second_response)]
+        },
+
+        fallbacks = [CommandHandler('stop', stop)]
+    )
+    application.add_handler(conv_handler)
     # Запускаем приложение.
     application.run_polling()
 
